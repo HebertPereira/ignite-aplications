5233664600f378f4b7c50840ce5b6214670aeb4e
HebertPereira <hebertpktd@gmail.com>
2025-03-12 16:24:34 -0300

    chore: completing 02 challenge

--- back-end/ignite-desafio-2/src/routes/diet.ts
+++ back-end/ignite-desafio-2/src/routes/diet.ts
@@ -5,7 +5,7 @@ import { randomUUID } from "node:crypto";
 
 export async function Diet(app: FastifyInstance) {
   app.post(
-    "/create-meal/:id",
+    "/create-meal/:authorId",
     async (request: FastifyRequest, response: FastifyReply) => {
       const getMealSchema = z.object({
         title: z.string(),
@@ -15,18 +15,18 @@ export async function Diet(app: FastifyInstance) {
       });
 
       const getParamsSchema = z.object({
-        id: z.string(),
+        authorId: z.string(),
       });
 
       const { title, description, on_diet, date } = getMealSchema.parse(
         request.body
       );
 
-      const { id } = getParamsSchema.parse(request.params);
+      const { authorId } = getParamsSchema.parse(request.params);
 
       await knex("diet").insert({
         id: randomUUID(),
-        author: id,
+        author: authorId,
         title,
         description,
         on_diet,
@@ -38,17 +38,67 @@ export async function Diet(app: FastifyInstance) {
   );
 
   app.get(
-    "/list-meals/:id",
+    "/list-meals/:authorId",
+    async (request: FastifyRequest, response: FastifyReply) => {
+      const getParamsSchema = z.object({
+        authorId: z.string(),
+      });
+
+      const { authorId } = getParamsSchema.parse(request.params);
+
+      const getMeals = await knex("diet").where("author", authorId).select();
+
+      return response.status(200).send({ meals: getMeals });
+    }
+  );
+
+  app.get(
+    "/list-meals/:authorId/:id",
     async (request: FastifyRequest, response: FastifyReply) => {
       const getParamsSchema = z.object({
         id: z.string(),
+        authorId: z.string(),
       });
 
-      const { id } = getParamsSchema.parse(request.params);
+      const { authorId, id } = getParamsSchema.parse(request.params);
 
-      const getMeals = await knex("diet").where("author", id).select();
+      const mealData = await knex("diet")
+        .where({ author: authorId, id })
+        .select();
 
-      return response.status(200).send({ meals: getMeals });
+      return response.status(200).send({ meal: mealData[0] });
+    }
+  );
+
+  app.put(
+    "/list-meals/:authorId/:id",
+    async (request: FastifyRequest, response: FastifyReply) => {
+      const getParamsSchema = z.object({
+        id: z.string(),
+        authorId: z.string(),
+      });
+
+      const { authorId, id } = getParamsSchema.parse(request.params);
+
+      const getBodySchema = z.object({
+        title: z.string(),
+        description: z.string(),
+        on_diet: z.boolean(),
+        date: z.string(),
+      });
+
+      const { title, description, on_diet, date } = getBodySchema.parse(
+        request.body
+      );
+
+      await knex("diet").where({ author: authorId, id }).update({
+        title,
+        description,
+        on_diet,
+        date,
+      });
+
+      return response.status(204).send();
     }
   );
 
@@ -64,7 +114,52 @@ export async function Diet(app: FastifyInstance) {
 
       await knex("diet").where({ author: authorId, id }).delete();
 
-      return response.status(200).send();
+      return response.status(204).send();
+    }
+  );
+
+  app.get(
+    "/summary/:authorId",
+    async (request: FastifyRequest, response: FastifyReply) => {
+      const getParamsSchema = z.object({
+        authorId: z.string(),
+      });
+
+      const { authorId } = getParamsSchema.parse(request.params);
+
+      const summaryDietMeals = await knex("diet")
+        .where({ author: authorId })
+        .select();
+
+      const numberOfMeals = summaryDietMeals.length;
+      const numberOfMealsOnDiet = summaryDietMeals.filter(
+        (res) => res.on_diet === 1
+      ).length;
+      const numberOfMealsNotOnDiet = summaryDietMeals.filter(
+        (res) => res.on_diet === 0
+      ).length;
+      let bestStreak = 0;
+      let lastStreak = 0;
+
+      summaryDietMeals.forEach((meal, index) => {
+        if (meal.on_diet) {
+          lastStreak = lastStreak + 1;
+          if (lastStreak > bestStreak) {
+            bestStreak = lastStreak;
+          }
+        } else {
+          lastStreak = 0;
+        }
+      });
+
+      return response.status(200).send({
+        diet: {
+          numberOfMeals,
+          onDiet: numberOfMealsOnDiet,
+          nonDiet: numberOfMealsNotOnDiet,
+          bestStreak,
+        },
+      });
     }
   );
 }
